2b11db54f0a1b1d65c69068b868deb06832f1b2d
Sascha Eggenberger <hello@saschaeggenberger.com>
2020-01-26 22:24:16 +0100

    linting fixes

--- src/Plugin/GraphQL/Enums/ResponsiveImageStyleId.php
+++ src/Plugin/GraphQL/Enums/ResponsiveImageStyleId.php
@@ -6,7 +6,9 @@ use Drupal\graphql\Plugin\GraphQL\Enums\EnumPluginBase;
 use Drupal\responsive_image\Entity\ResponsiveImageStyle;
 
 /**
- * @GraphQLEnum(
+ * EnumPlugin.
+ *
+ * GraphQLEnum(
  *   id = "responsive_image_style_id",
  *   name = "ResponsiveImageStyleId",
  *   provider = "image"
@@ -14,6 +16,9 @@ use Drupal\responsive_image\Entity\ResponsiveImageStyle;
  */
 class ResponsiveImageStyleId extends EnumPluginBase {
 
+  /**
+   * Public Function buildEnumValues()
+   */
   public function buildEnumValues($definition) {
     $items = [];
     foreach (ResponsiveImageStyle::loadMultiple() as $responsiveImageStyle) {
@@ -25,4 +30,5 @@ class ResponsiveImageStyleId extends EnumPluginBase {
     }
     return $items;
   }
+
 }
--- src/Plugin/GraphQL/Fields/Entity/Fields/Image/ResponsiveImage.php
+++ src/Plugin/GraphQL/Fields/Entity/Fields/Image/ResponsiveImage.php
@@ -1,13 +1,12 @@
 <?php
+
 namespace Drupal\graphql_responsive_image\Plugin\GraphQL\Fields\Entity\Fields\Image;
 
-use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\graphql\GraphQL\Execution\ResolveContext;
 use Drupal\graphql\Plugin\GraphQL\Fields\FieldPluginBase;
 use Drupal\image\Plugin\Field\FieldType\ImageItem;
-use Drupal\responsive_image\Entity\ResponsiveImageStyle;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use GraphQL\Type\Definition\ResolveInfo;
 
@@ -35,6 +34,7 @@ class ResponsiveImage extends FieldPluginBase implements ContainerFactoryPluginI
    * @var \Drupal\Core\Render\RendererInterface
    */
   protected $renderer;
+
   /**
    * Constructs an ImageResponsive object.
    *
@@ -47,10 +47,11 @@ class ResponsiveImage extends FieldPluginBase implements ContainerFactoryPluginI
    * @param \Drupal\Core\Render\RendererInterface $renderer
    *   The renderer service.
    */
-  public function __construct(array $configuration, $pluginId, $pluginDefinition, RendererInterface $renderer) {
+  public function __construct(array $configuration, $pluginId, array $pluginDefinition, RendererInterface $renderer) {
     $this->renderer = $renderer;
     parent::__construct($configuration, $pluginId, $pluginDefinition);
   }
+
   /**
    * {@inheritdoc}
    */
@@ -62,6 +63,7 @@ class ResponsiveImage extends FieldPluginBase implements ContainerFactoryPluginI
         $container->get('renderer')
     );
   }
+
   /**
    * {@inheritdoc}
    */
@@ -69,9 +71,9 @@ class ResponsiveImage extends FieldPluginBase implements ContainerFactoryPluginI
     if ($value instanceof ImageItem && $value->entity->access('view')) {
 
       $variables = [
-          '#theme' => 'responsive_image',
-          '#responsive_image_style_id' => $args['style'],
-          '#uri' => $value->entity->getFileUri(),
+        '#theme' => 'responsive_image',
+        '#responsive_image_style_id' => $args['style'],
+        '#uri' => $value->entity->getFileUri(),
       ];
 
       yield $this->renderer->render($variables);
